5237a480949339bc88f8233793bad8e8054ef7f6
Angel Escalante <a.fer.esca@gmail.com>
2020-09-26 22:24:26 -0500

    Add docs

---
+++ docs/precache-manifest.01b8f5e0d54512155e9ea585b96c8101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c135aea607cf806c4479b3d94e6e5417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38e6dd1b4f556977d382",
+    "url": "/static/css/main.9ce4aa95.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e6dd1b4f556977d382",
+    "url": "/static/js/main.f9dd9b5c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
